977cd7eb6411407b06660294f5770826036cf679
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-06-08 14:57:33 -0600

    Complete exercise 1

---
+++ pairing_2/friends.rb
@@ -0,0 +1,9 @@
+arrays = ["Alaina", "Becka", "Andrew", "Madeline", "Corey"]
+
+p arrays[2]
+p arrays[4]
+p arrays[0]
+
+places = ["Florence", "Costa Rica", "Paris", "New York", "Albuquerque"]
+
+p places.pop
